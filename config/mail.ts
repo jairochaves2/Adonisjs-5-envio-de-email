8e600715a2247eaa89eb828b9b37cc57d11a4af1
Jairo Chaves <jairochaves2@gmail.com>
2021-10-27 19:59:02 -0300

    a

--- config/mail.ts
+++ config/mail.ts
@@ -46,13 +46,13 @@ const mailConfig: MailConfig = {
       driver: 'smtp',
       host: Env.get('SMTP_HOST'),
       port: Env.get('SMTP_PORT'),
-			auth: {
-				user: Env.get('SMTP_USERNAME'),
-				pass: Env.get('SMTP_PASSWORD'),
-				type: 'login',
-			}
+      secure: false,
+      auth: {
+        user: Env.get('SMTP_USERNAME'),
+        pass: Env.get('SMTP_PASSWORD'),
+        type: 'login',
+      },
     },
-
   },
 }
 
